e3478105292213a6af0263f67f6b304979be3be1
Navvya21 <75784090+Navvya21@users.noreply.github.com>
2022-01-14 17:25:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,13 +13,30 @@ function modelLoaded() {
     console.log("PoseNet is initialized!");
 }
 
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
 function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX" + noseX + "noseY" + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX- rightWristX);
     }
 }
 
 function draw(){
     background("#CEA1DF");
+    document.getElementById("square_side").innerHTML = "Width and height will be-" + difference + "px";
+    fill("#81e3ee");
+    stroke("#b09ce5");
+    square(noseX, noseY, difference);
 }
